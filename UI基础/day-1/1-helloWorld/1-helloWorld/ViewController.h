74f15199d661314556f1ec295b4db88d683561c0
gouzi <gouzi@wangjun.local>
2017-03-10 10:45:53 +0800

    第一次创建———iOS再学习笔记

---
+++ UI基础/day-1/1-helloWorld/1-helloWorld/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  1-helloWorld
+//
+//  Created by gouzi on 2017/3/10.
+//  Copyright © 2017年 wangjun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ UI基础/day-1/1-helloWorld/1-helloWorld/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  1-helloWorld
+//
+//  Created by gouzi on 2017/3/10.
+//  Copyright © 2017年 wangjun. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
