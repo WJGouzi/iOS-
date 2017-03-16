02cc1ee1659cf2a4f5e8391fefa77826e5177101
gouzi <gouzi@wangjun.local>
2017-03-16 15:10:26 +0800

    字典转模型

---
+++ UI基础/day-4/2-模型取代字典/2-shoppingCart/shop.m
@@ -0,0 +1,13 @@
+//
+//  shop.m
+//  2-shoppingCart
+//
+//  Created by gouzi on 2017/3/16.
+//  Copyright © 2017年 wangjun. All rights reserved.
+//
+
+#import "shop.h"
+
+@implementation shop
+
+@end
