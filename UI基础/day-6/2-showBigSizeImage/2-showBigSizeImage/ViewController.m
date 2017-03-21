22d0273ebd19a9eba8b8576a730e6efc9a2413b1
gouzi <gouzi@wangjun.local>
2017-03-21 17:56:57 +0800

    scrollView的一些基本属性

--- UI基础/day-6/2-showBigSizeImage/2-showBigSizeImage/ViewController.m
+++ UI基础/day-6/2-showBigSizeImage/2-showBigSizeImage/ViewController.m
@@ -23,7 +23,35 @@
 //    CGFloat imageW = image.size.width;
 //    CGFloat imageH = image.size.height;
     self.scrollView.contentSize = image.size;//CGSizeMake(imageW, imageH);
+    
+    // 常见的基本属性
+    self.scrollView.bounces = NO; // 回弹效果
+    
+//    [self indicatorView];
+    
+    // 滚动条
+    self.scrollView.showsVerticalScrollIndicator = YES;
+    self.scrollView.showsHorizontalScrollIndicator = YES;
+    
+    
 }
 
+
+// 设置菊花
+- (void)indicatorView {
+    /*
+     UIActivityIndicatorViewStyleWhiteLarge,
+     UIActivityIndicatorViewStyleWhite,
+     UIActivityIndicatorViewStyleGray
+     */
+    UIActivityIndicatorView *indicatorView = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleGray];
+    indicatorView.center = CGPointMake(100, -40);
+    [indicatorView startAnimating];
+    [self.scrollView addSubview:indicatorView];
+    
+    // 只有在没有设置从contentSize的时候才会有用
+    self.scrollView.alwaysBounceVertical = YES; // 不能进行滚动，只是有个弹簧效果而已
+    self.scrollView.alwaysBounceHorizontal = NO;
+}
 
 @end
