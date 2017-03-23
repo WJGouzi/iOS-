0a71a42859b22f04561840fb7dfc2ecb4eeff99c
gouzi <gouzi@MacBook-Pro.local>
2017-03-23 15:41:55 +0800

    用代码进行设置autoresizing

--- UI基础/day-7/2-autoResizeUse/2-autoResizeUse/ViewController.m
+++ UI基础/day-7/2-autoResizeUse/2-autoResizeUse/ViewController.m
@@ -22,6 +22,7 @@
  */
 
 
+#pragma mark - 利用xib进行设置
 /*  autoresizing的设置方法
  *  在storyboard中->show the siee inspector -> view -> autoresizing -> 内部的六根线
  *  注意点:
@@ -31,8 +32,31 @@
  *  2> 内部的两根线选择代表子控件的宽高会跟随着父控件的大小而进行伸缩
  */
 
+
+
+#pragma mark - 利用代码进行设置
+/*
+ *  需求:子控件在父控件右下角，不管屏幕尺寸的变化，都只显示在右下角(就代表是距离左边和上边是可以伸缩的)
+ */
 - (void)viewDidLoad {
     [super viewDidLoad];
+    UIView *redView = [[UIView alloc] init];
+    redView.backgroundColor = [UIColor redColor];
+    CGFloat originX = self.view.frame.size.width - 100;
+    CGFloat originY = self.view.frame.size.height - 100;
+    redView.frame = CGRectMake(originX, originY, 100, 100);
+    
+    
+    /*
+     *  UIViewAutoresizingFlexibleLeftMargin        距离父控件的左边是可以伸缩的
+     *  UIViewAutoresizingFlexibleBottomMargin      距离父控件的底部是可以伸缩的
+     *  UIViewAutoresizingFlexibleRightMargin       距离父控件的右边是可以伸缩的
+     *  UIViewAutoresizingFlexibleTopMargin         距离父控件的顶部是可以伸缩的
+     *  UIViewAutoresizingFlexibleWidth             宽度会跟随父控件的宽度进行伸缩
+     *  UIViewAutoresizingFlexibleHeight            高度会跟随父控件的高度进行伸缩
+     */
+    redView.autoresizingMask = UIViewAutoresizingFlexibleLeftMargin | UIViewAutoresizingFlexibleTopMargin;
+    [self.view addSubview:redView];
     
 }
 
