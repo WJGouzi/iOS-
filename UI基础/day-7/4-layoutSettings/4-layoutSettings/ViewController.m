63cfb3bdb005a04d502bbed1cf645460a3233a5a
gouzi <gouzi@MacBook-Pro.local>
2017-03-26 17:08:34 +0800

    父控件的高度随着子控件的内容进行变化。

---
+++ UI基础/day-7/4-layoutSettings/4-layoutSettings/ViewController.m
@@ -0,0 +1,31 @@
+//
+//  ViewController.m
+//  4-layoutSettings
+//
+//  Created by gouzi on 2017/3/26.
+//  Copyright © 2017年 王钧. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+
+/**
+ *  需求:
+ *  > 父控件的高度随着子控件的内容进行伸缩。
+ */
+
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+
+
+
+@end
