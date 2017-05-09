20e67402afbf2f9fd59933f0edf880425de4ddce
gouzi <gouzi@wangjun.local>
2017-05-09 14:52:57 +0800

    storyboard中自定义静态cell

---
+++ UI基础/day-9/8-静态cell/8-静态cell/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  8-静态cell
+//
+//  Created by gouzi on 2017/5/9.
+//  Copyright © 2017年 wj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ UI基础/day-9/8-静态cell/8-静态cell/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  8-静态cell
+//
+//  Created by gouzi on 2017/5/9.
+//  Copyright © 2017年 wj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
