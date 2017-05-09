fd6e430282254523567e91faa170b2e1467af805
gouzi <gouzi@wangjun.local>
2017-05-09 14:53:44 +0800

    静态cell  storyboard

--- UI基础/day-9/8-静态cell/8-静态cell/ViewController.m
+++ UI基础/day-9/8-静态cell/8-静态cell/ViewController.m
@@ -4,7 +4,8 @@
 //
 //  Created by gouzi on 2017/5/9.
 //  Copyright © 2017年 wj. All rights reserved.
-//
+//  界面的数据可以定下来的时候，就可以使用静态cell，基本上都是放在storyboard中
+
 
 #import "ViewController.h"
 
@@ -16,13 +17,14 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
 }
 
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+#pragma mark - 代理方法
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    NSLog(@"%ld---%ld", indexPath.section, indexPath.row);
 }
 
 
