d3fbbb769ab62b1c68e0c8b74674d93576db4b63
gouzi <gouzi@wangjun.local>
2017-05-12 14:54:18 +0800

    清空购物车和购买按钮的点击事件已经完成

--- UI基础/day-11/1-购物车/1-购物车/ViewController.m
+++ UI基础/day-11/1-购物车/1-购物车/ViewController.m
@@ -20,6 +20,8 @@
 
 @property (weak, nonatomic) IBOutlet UILabel *totalPriceLabel;
 
+@property (weak, nonatomic) IBOutlet UIButton *buyBtn;
+@property (weak, nonatomic) IBOutlet UIButton *clearBtn;
 
 @end
 
@@ -35,33 +37,11 @@
     
 }
 
+
+
 - (void)dealloc {
     [[NSNotificationCenter defaultCenter] removeObserver:self];
 }
-
-
-#pragma mark - 监听通知
-- (void)plusClick:(NSNotification *)notification {
-    NSLog(@"click plus");
-    wjShopCarCell *cell = notification.object;
-    
-    int totalPrice = [self.totalPriceLabel.text intValue] + cell.model.money.intValue;
-    
-    self.totalPriceLabel.text = [NSString stringWithFormat:@"%d", totalPrice];
-    
-}
-
-- (void)minusClick:(NSNotification *)notification {
-    NSLog(@"click minus");
-    wjShopCarCell *cell = notification.object;
-    
-    int totalPrice = [self.totalPriceLabel.text intValue] - cell.model.money.intValue;
-    
-    self.totalPriceLabel.text = [NSString stringWithFormat:@"%d", totalPrice];
-    
-}
-
-
 
 
 #pragma mark - 获取数据源
@@ -72,6 +52,60 @@
     return _wineArray;
 }
 
+
+
+
+#pragma mark - 监听通知
+- (void)plusClick:(NSNotification *)notification {
+    wjShopCarCell *cell = notification.object;
+    
+    int totalPrice = [self.totalPriceLabel.text intValue] + cell.model.money.intValue;
+    
+    self.totalPriceLabel.text = [NSString stringWithFormat:@"%d", totalPrice];
+    
+    self.buyBtn.enabled = YES;
+    self.clearBtn.enabled = YES;
+}
+
+- (void)minusClick:(NSNotification *)notification {
+    wjShopCarCell *cell = notification.object;
+    
+    int totalPrice = [self.totalPriceLabel.text intValue] - cell.model.money.intValue;
+    
+    self.totalPriceLabel.text = [NSString stringWithFormat:@"%d", totalPrice];
+    
+    if (totalPrice == 0) {
+        self.buyBtn.enabled = NO;
+        self.clearBtn.enabled = NO;
+    }
+    
+}
+
+
+
+#pragma mark - 按钮的点击事件
+- (IBAction)clearShopCar {
+    self.totalPriceLabel.text = @"0";
+    
+    // 拿到模型
+    for (wjShopCarModel *model in self.wineArray) {
+        model.count = 0;
+    }
+    // 刷新tableView
+    [self.tableView reloadData];
+    self.clearBtn.enabled = NO;
+    self.buyBtn.enabled = NO;
+}
+
+- (IBAction)buyProduct {
+    // 打印购买了写什么东西
+    for (wjShopCarModel *model in self.wineArray) { // 性能有问题的
+        if (model.count) {
+            NSLog(@"够了%d瓶%@牌的酒", model.count, model.name);
+        }
+    }
+    NSLog(@"总价为:%@",self.totalPriceLabel.text);
+}
 
 
 #pragma mark - UITableViewDataSource
@@ -87,6 +121,8 @@ static NSString *iden = @"shopCarCell";
     
     return cell;
 }
+
+
 
 
 
